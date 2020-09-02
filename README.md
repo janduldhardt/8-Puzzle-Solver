0f08a05ad437dc10d622d0814d1d6d919dbdef71
Jan Duldhardt <jan.duldhardt@ebcsoft.de>
2020-09-02 18:10:10 +0700

    Add user input solver

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # 8-Puzzle-Solver
+
+For random generated puzzles execute main.py
+For solve puzzle via user input execute main2.py
---
+++ main2.py
@@ -0,0 +1,256 @@
+from puzzlegen import generate_puzzle
+import heapq
+import time
+
+# algo = 1 #manhattan
+# algo = 2 #RowCOl-dist
+# algo = 3 #Misplaced
+algoDict = {1 : 'Manhattan', 2:'RowColDif', 3 : 'MisplacedTiles'}
+algoList = [1,2,3]
+
+def manhattan_dist(pt1, pt2):
+    distance = 0
+    for i in range(len(pt1)):
+        distance += abs(pt1[i] - pt2[i])
+    return distance
+
+class MyClass:
+    def __init__(self, puzzle,g):
+        self.puzzle = puzzle
+        row, col = self.find_empty()
+        self.row = row
+        self.col = col
+        self.g = g
+        self.prev = None
+        self.finish = False
+
+
+    def find_empty(self):
+        for i in range(len(self.puzzle)):
+            for j in range(len(self.puzzle[i])):
+                if self.puzzle[i][j] == 0:
+                    return i, j
+
+    def f(self):
+        return self.g + self.get_h()
+    
+    def get_h(self):
+        if algo == 1:
+            return self.get_manhattan()
+        if algo == 2:
+            return self.get_rowcoldif()
+        if algo == 3:
+            return self.get_misplaced()
+
+    def print(self):
+        for i in self.puzzle:
+            print(i)
+    
+    def get_children(self):
+            actions = [(1,0),(0,1),(-1,0),(0,-1)]
+            children = []
+            for a in actions:
+                row = self.row + a[0]
+                col = self.col + a[1]
+                if row < 0 or row > 2 or col < 0 or col > 2:
+                    continue
+
+                
+                m = [puzzlerow[:] for puzzlerow in self.puzzle]
+                temp = m[row][col]
+                m[row][col] = 0
+                m[self.row][self.col] = temp
+                # if self.check_puzzle_in_path(m):
+                #     continue
+
+                if self.check_puzzle_in_closed(m,closedlist):
+                    continue
+
+                ns = MyClass(m,self.g+1)
+                ns.prev = self
+                children.append(ns)
+            
+            return children
+
+
+    def get_misplaced(self):
+        puzzle = self.puzzle
+        count = 0
+        for i in range(len(puzzle)):
+            for j in range(len(puzzle[i])):
+                if puzzle[i][j] != goal[i][j] and puzzle[i][j] != 0:
+                    count += 1
+        
+        if count == 0:
+            self.finish = True
+        return count
+
+    def check_puzzle_in_path(self, matrix):
+        state = self
+        while state.prev != None:
+            if state.puzzle == matrix:
+                return True
+            state = state.prev
+
+        return False
+
+    def check_puzzle_in_closed(self, m, closedlist):
+        # for x in closedlist:
+        #     if x == m:
+        #         return True
+
+        return matrixtostring(m) in closedlist
+
+    def get_manhattan(self):
+        state = self.puzzle
+        distances = [[0,0,0] for i in range(3)]
+        for i in range(len(state)):
+            for j in range(len(state[i])):
+                no = state[i][j]
+                row, col = dicts[str(no)]
+                distances[i][j] = manhattan_dist([i,j],[row,col])
+
+        msum = 0
+        for i in range(len(distances)):
+            for j in range(len(distances[i])):
+                msum += distances[i][j]
+        
+        if msum == 0:
+            self.finish = True
+        return msum
+
+    def get_rowcoldif(self):
+        state = self.puzzle
+        distances = [[0,0,0] for i in range(3)]
+        for i in range(len(state)):
+            for j in range(len(state[i])):
+                score = 0
+                no = state[i][j]
+                row, col = dicts[str(no)]
+                if i != row:
+                    score += 1
+                if j != col:
+                    score += 1
+                distances[i][j] = score
+
+        msum = 0
+        for i in range(len(distances)):
+            for j in range(len(distances[i])):
+                msum += distances[i][j]
+        
+        if msum == 0:
+            self.finish = True
+        return msum
+
+    def print_path(self):
+        state = self
+        sl = []
+        while state != None:
+            sl.append(state)
+            state = state.prev
+        while len(sl) > 1:
+            state = sl.pop()
+            state.print()
+            print('    |')
+            print('    |')
+            print('    V')
+        state = sl.pop()
+        state.print()
+        
+        print("Tree depth: ", self.g)
+    
+def matrixtostring(matrix):
+    s ='' 
+    for i in range(len(matrix)):
+        for j in range(len(matrix)):
+            s += (str(matrix[i][j]))
+    
+    return s
+
+
+
+def get_next(openlist):
+    best = openlist[0]
+    bestv = best.f()
+    for x in openlist:
+        if x.f() < bestv:
+            best = x
+            bestv = x.f()
+    return best
+        
+def create_dict(gs):
+    mydict = {}
+    for i in range(len(gs)):
+        for j in range(len(gs[i])):
+            mydict[str(gs[i][j])] = (i,j)
+
+    return mydict
+
+def stringtopuzzle(pstr):
+    lst = []
+    for i in range(len(pstr)):
+        lst.insert(0,int(pstr[i]))
+
+    pzl = []
+    for i in range(3):
+        row = []
+        row.append(lst.pop())
+        row.append(lst.pop())
+        row.append(lst.pop())
+        pzl.append(row)
+        
+    print(pzl)
+    return pzl
+
+print("Please enter the init state eg: 012345678")
+puzzlelist = input().strip()
+puzzle = stringtopuzzle(puzzlelist)
+
+dummy = 0
+
+
+for algo in algoList:
+    # S = MyClass([[1,2,5],[6,3,0],[4,7,8]],0)
+    #S = MyClass([[1,2,3],[4,5,0],[6,7,8]],0) #13 steps to goal
+    # S = MyClass([[8,6,7],[2,5,4],[3,0,1]],0)
+    S = MyClass(puzzle,0)
+    goal = [[1,2,3],[4,5,6],[7,8,0]]
+    dicts = create_dict(goal)
+
+    openl = [(S.f(),dummy,S)]
+    dummy += 1
+    closedlist = {} 
+
+    start = time.time()
+    for i in range(1000000):
+        # print(i)
+
+        if S.finish == True:
+            # print("Goal!!")
+            break
+        # openl.remove(S)
+        closedlist[matrixtostring(S.puzzle)] = 1
+        children = S.get_children()
+        # openl.extend(children)
+        for x in children:
+            heapq.heappush(openl,(x.f(),dummy,x))
+            dummy += 1
+
+
+        S = heapq.heappop(openl)[2]
+
+    end = time.time()
+
+    # print(i)
+    # print(S.g)
+    alltime = end - start
+    if i > 179997:
+        break
+
+    algoName = str(algoDict[algo])
+    print(algoName + " time: "+str(alltime))
+    print(algoName + " nodes expanded: "+str(i))
+
+
+
+S.print_path()
